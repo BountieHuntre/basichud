f39dc4dbde553f659e5e2f388fa3e2e56681f73a
deadremains45 <nick_puppydog@yahoo.com>
2014-07-07 21:13:58 -0500

    added files

---
+++ addons/darkrpmodification/lua/darkrp_modules/extrafonts/sv_font.lua
@@ -0,0 +1,4 @@
+resource.AddFile("resource/fonts/BebasNeue.otf")
+resource.AddFile("resource/fonts/akbar.ttf")
+resource.AddFile("resource/fonts/minecraftia.ttf")
+resource.AddFile("resource/fonts/American Captain.otf")
